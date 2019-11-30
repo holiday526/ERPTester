2841eb9cb71c4e8f8ebe6eb701fc0d3369a72f89
holiday526 <taylorchan526@gmail.com>
2019-11-30 17:35:31 +0800

    upload bootstrap vendor

--- resources/views/layout/partial/footer-scripts.blade.php
+++ resources/views/layout/partial/footer-scripts.blade.php
@@ -7,7 +7,7 @@
 <script src="{{asset('vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
 
 <!-- Custom scripts for all pages-->
-<script src="{{asset('js/sb-admin-2.min.js')}}"></script>
+<script src="{{ asset('js/sb-admin-2.min.js') }}"></script>
 
 <!-- Core plugin JavaScript-->
 <script src="{{asset('vendor/jquery-easing/jquery.easing.min.js')}}"></script>
